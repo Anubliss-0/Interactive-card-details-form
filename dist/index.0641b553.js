ede7af2ca7a3f05d2486e298faf62c7a8bedebbf
Thomsd Rice <tom@anubliss.com>
2022-10-13 19:03:51 -0400

    Length verification added to fields, number fields changed to tel format

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -544,6 +544,8 @@ const month = document.querySelector("#month");
 const year = document.querySelector("#year");
 const cardNum = document.querySelector("#card-num");
 const cvc = document.querySelector("#cvc");
+const monthInvalid = document.querySelector("#month-invalid");
+const yearInvalid = document.querySelector("#year-invalid");
 function validate() {
     console.log("firing");
     inputs.forEach((input)=>input.setAttribute("required", "required"));
@@ -551,13 +553,27 @@ function validate() {
     month.setAttribute("minlength", "2");
     year.setAttribute("minlength", "2");
     cvc.setAttribute("minlength", "3");
+    if (month.value.length == 0) monthInvalid.innerText = "Can't be blank";
+    if (year.value.length == 0) yearInvalid.innerText = "Can't be blank";
 }
 button.addEventListener("click", validate);
+cardNum.addEventListener("keyup", (event)=>{
+    cardNum.value = cardNum.value.replace(/.{4}/g, "$&' '");
+});
 month.addEventListener("keyup", (event)=>{
-    if (month.value.length == 2) {
-        console.log("testing!");
-        month.nextElementSibling.focus();
-    }
+    if (month.value.length >= 1) monthInvalid.innerText = "";
+    if (month.value.length == 2) month.nextElementSibling.focus();
+});
+month.addEventListener("keypress", (event)=>{
+    const REGEXP = /[0-9\/]+/;
+    if (!REGEXP.test(event.key)) event.preventDefault();
+});
+year.addEventListener("keyup", (event)=>{
+    if (year.value.length >= 1) yearInvalid.innerText = "";
+});
+year.addEventListener("keypress", (event)=>{
+    const REGEXP = /[0-9\/]+/;
+    if (!REGEXP.test(event.key)) event.preventDefault();
 });
 
 },{}],"2jwzK":[function(require,module,exports) {
